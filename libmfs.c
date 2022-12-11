b1b916bd5fb6cbecc7bf0715b5df6b7ca5eb2caf
kinjjalmehta <kkmehta3@wisc.edu>
2022-12-11 13:43:40 -0600

    update libmfs.c

--- libmfs.c
+++ libmfs.c
@@ -5,6 +5,7 @@
 
 #define BUFFER_SIZE (10000)
 int sd; 
+
 int MFS_Init(char *hostname, int port) {
     printf("MFS Init2 %s %d\n", hostname, port);
     struct sockaddr_in addrSnd, addrRcv;
@@ -53,10 +54,43 @@ int MFS_Lookup(int pinum, char *name) {
     return m.inum;
 }
 
+/**
+ * Returns some information about the file specified by inum.
+ * Success: return 0; Failure: return -1
+ * The exact info returned is defined by `MFS_Stat_t`.
+ * Failure modes: inum does not exist
+ */
 int MFS_Stat(int inum, MFS_Stat_t *m) {
-    return 0;
+    message_t info;
+    //message_t
+    struct sockaddr_in addrSnd, addrRcv;
+    info.mtype= MFS_STAT;
+    info.inum = inum;
+
+    info.rc = UDP_Write(sd, &addrSnd, (char *) &info, sizeof(message_t));
+    if (info.rc < 0) {
+	    printf("client:: failed to send\n");
+	    exit(1);
+    }
+
+    printf(" NEW client:: wait for reply...\n");
+    info.rc = UDP_Read(sd, &addrRcv, (char *) &info, sizeof(message_t));
+    printf("client:: got reply [size:%d contents:(%d)\n", info.rc, info.mtype);
+    if (info.rc < 0){
+        return -1;
+    }
+    else {
+        info.stat.size = m->size;
+        info.stat.type = m->type;
+        return 0;
+    }
+    return -1;
 }
 
+/**
+ * Writes a buffer of size nbytes (max: 4096 bytes) at the byte offset specified by offset.
+ * Success: return 0; Failure: return -1
+*/
 int MFS_Write(int inum, char *buffer, int offset, int nbytes) {
     return 0;
 }
--- message.h
+++ message.h
@@ -18,6 +18,7 @@ typedef struct {
     int inum;
     char * name;
     // put more here ...
+    MFS_Stat_t stat;
 } message_t;
 
 #endif // __message_h__
